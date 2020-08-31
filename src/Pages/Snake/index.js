4093c5a517255af102399d66c8392d3be7032c82
Bruno Alves <bruno@smn.com.br>
2020-08-31 20:43:01 -0300

    snake

--- src/Pages/Snake/index.js
+++ src/Pages/Snake/index.js
@@ -1,16 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Table } from './styles';
 
 function Snake() {
+  const [config] = useState({ ammountTR: 15, ammountTD: 15 });
+  const [snake, setSnake] = useState(
+    Array.from(Array(config.ammountTR), () =>
+      Array.from(Array(config.ammountTD))
+    )
+  );
+
+  console.log(snake);
+
   return (
-    <table>
+    <Table>
       <tbody>
-        <tr>
-          <td />
-          <td />
-          <td />
-        </tr>
+        {Array.from(Array(config.ammountTR).keys()).map((i) => (
+          <tr key={String(i)}>
+            {Array.from(Array(config.ammountTD).keys()).map((d) => {
+              return <td key={String(d)} className="tail" />;
+            })}
+          </tr>
+        ))}
       </tbody>
-    </table>
+    </Table>
   );
 }
 
---
+++ src/Pages/Snake/styles.js
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+
+export const Table = styled.table`
+  width: 800px;
+  height: 400px;
+  border-collapse: collapse;
+  background: rgb(80, 80, 80);
+
+  .head {
+    background: red;
+  }
+
+  .body {
+    background: white;
+  }
+
+  .tail {
+    background: yellow;
+  }
+`;
