504811882486e0d406f016c093cd735039377341
Bruno Alves <bruno@smn.com.br>
2020-09-11 20:05:49 -0300

    alteração de metodos

--- src/Pages/Snake/index.js
+++ src/Pages/Snake/index.js
@@ -5,7 +5,42 @@ function Snake() {
   const [snake, setSnake] = useState([0, 1, 2]);
   const [direction, setDirection] = useState('D');
 
-  /*
+  useEffect(() => {
+    const changeDirection = (e) => {
+      switch (e.keyCode) {
+        case 87:
+          setDirection('W');
+          break;
+        case 65:
+          setDirection('A');
+          break;
+        case 83:
+          setDirection('S');
+          break;
+        case 68:
+          setDirection('D');
+          break;
+        default:
+      }
+    };
+
+    document.addEventListener('keydown', changeDirection);
+    return () => document.removeEventListener('keydown', changeDirection);
+  }, []);
+
+  const setClassName = (x, y) => {
+    const p = x * 15 + y;
+
+    if (snake.includes(p)) {
+      if (snake[0] === p) return 'tail';
+      if (snake[snake.length - 1] === p) return 'head';
+
+      return 'body';
+    }
+
+    return undefined;
+  };
+
   const move = () => {
     const s = [...snake];
     setSnake(
@@ -27,70 +62,22 @@ function Snake() {
       })
     );
   };
-  */
-
-  const changeDirection = (e) => {
-    switch (e.keyCode) {
-      case 87:
-        setDirection('W');
-        break;
-      case 65:
-        setDirection('A');
-        break;
-      case 83:
-        setDirection('S');
-        break;
-      case 68:
-        setDirection('D');
-        break;
-      default:
-    }
-  };
-
-  const setClassName = (position) => {
-    if (snake[0] === position) return 'tail';
-    if (snake[snake.length - 1] === position) return 'head';
-
-    return snake.includes(position) ? 'body' : undefined;
-  };
 
   useEffect(() => {
-    document.addEventListener('keydown', changeDirection);
-  }, []);
-
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      const s = [...snake];
-      setSnake(
-        s.map((_, i) => {
-          if (s.length === i + 1) {
-            switch (direction) {
-              case 'W':
-                return s[i] - 15;
-              case 'A':
-                return s[i] - 1;
-              case 'S':
-                return s[i] + 15;
-              case 'D':
-                return s[i] + 1;
-              default:
-            }
-          }
-          return s[i + 1];
-        })
-      );
+    const timer = setInterval(() => {
+      move();
     }, 1000);
 
-    return () => clearTimeout(timer);
-  }, [snake]);
+    return () => clearInterval(timer);
+  });
 
   return (
     <Table>
       <tbody>
-        {Array.from(Array(15), () => Array.from(Array(15))).map((tr, x) => (
+        {Array.from(Array(15)).map((_, x) => (
           <tr key={x.toString()}>
-            {tr.map((_, y) => (
-              <td key={y.toString()} className={setClassName(x * 15 + y)} />
+            {Array.from(Array(15)).map((__, y) => (
+              <td key={y.toString()} className={setClassName(x, y)} />
             ))}
           </tr>
         ))}
